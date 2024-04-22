dfc3d21b7224ad1e6c972f65a575fff5ff3fd845
jureivsek <jure.ivsek@student.fmf.uni-lj.si>
2024-04-22 11:24:47 +0200

    Osnove: števec klikov

--- StevecKlikov/Form1.Designer.cs
+++ StevecKlikov/Form1.Designer.cs
@@ -28,13 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label_counter = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label_counter
+            // 
+            this.label_counter.AutoSize = true;
+            this.label_counter.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label_counter.Location = new System.Drawing.Point(156, 183);
+            this.label_counter.Name = "label_counter";
+            this.label_counter.Size = new System.Drawing.Size(452, 63);
+            this.label_counter.TabIndex = 0;
+            this.label_counter.Text = "ŠTEVEC KLIKOV";
+            this.label_counter.Click += new System.EventHandler(this.label_counter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label_counter);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label_counter;
     }
 }
 
--- StevecKlikov/Form1.cs
+++ StevecKlikov/Form1.cs
@@ -12,9 +12,21 @@ namespace StevecKlikov
 {
     public partial class Form1 : Form
     {
+        int counter = 0;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            label_counter.Text = $"ŠTEVEC KLIKOV {counter++}";
+        }
+
+        private void label_counter_Click(object sender, EventArgs e)
+        {
+            label_counter.Text = $"ŠTEVEC KLIKOV {counter++}";
         }
     }
 }
