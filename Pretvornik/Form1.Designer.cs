71332d4752ad4ba863c4405cb76e75c9ba28ac6d
jureivsek <jure.ivsek@student.fmf.uni-lj.si>
2024-04-30 19:26:11 +0200

    Pretvornik - osnovno

---
+++ Pretvornik/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Pretvornik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_pretvori = new System.Windows.Forms.Button();
+            this.tb_cm = new System.Windows.Forms.TextBox();
+            this.tb_ft = new System.Windows.Forms.TextBox();
+            this.tb_inch = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btn_pocisti = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_pretvori
+            // 
+            this.btn_pretvori.Location = new System.Drawing.Point(311, 134);
+            this.btn_pretvori.Name = "btn_pretvori";
+            this.btn_pretvori.Size = new System.Drawing.Size(105, 34);
+            this.btn_pretvori.TabIndex = 0;
+            this.btn_pretvori.Text = "PRETVORI";
+            this.btn_pretvori.UseVisualStyleBackColor = true;
+            this.btn_pretvori.Click += new System.EventHandler(this.btn_pretvori_Click);
+            // 
+            // tb_cm
+            // 
+            this.tb_cm.Location = new System.Drawing.Point(122, 148);
+            this.tb_cm.Multiline = true;
+            this.tb_cm.Name = "tb_cm";
+            this.tb_cm.Size = new System.Drawing.Size(97, 20);
+            this.tb_cm.TabIndex = 1;
+            this.tb_cm.TextChanged += new System.EventHandler(this.tb_cm_TextChanged);
+            // 
+            // tb_ft
+            // 
+            this.tb_ft.Location = new System.Drawing.Point(505, 148);
+            this.tb_ft.Multiline = true;
+            this.tb_ft.Name = "tb_ft";
+            this.tb_ft.Size = new System.Drawing.Size(97, 20);
+            this.tb_ft.TabIndex = 2;
+            this.tb_ft.TextChanged += new System.EventHandler(this.tb_ft_TextChanged);
+            // 
+            // tb_inch
+            // 
+            this.tb_inch.Location = new System.Drawing.Point(639, 148);
+            this.tb_inch.Multiline = true;
+            this.tb_inch.Name = "tb_inch";
+            this.tb_inch.Size = new System.Drawing.Size(97, 20);
+            this.tb_inch.TabIndex = 3;
+            this.tb_inch.TextChanged += new System.EventHandler(this.tb_inch_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(118, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "cm";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(635, 111);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "inch";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(501, 111);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "ft";
+            // 
+            // btn_pocisti
+            // 
+            this.btn_pocisti.Location = new System.Drawing.Point(325, 174);
+            this.btn_pocisti.Name = "btn_pocisti";
+            this.btn_pocisti.Size = new System.Drawing.Size(75, 23);
+            this.btn_pocisti.TabIndex = 9;
+            this.btn_pocisti.Text = "POČISTI";
+            this.btn_pocisti.UseVisualStyleBackColor = true;
+            this.btn_pocisti.Click += new System.EventHandler(this.btn_pocisti_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 226);
+            this.Controls.Add(this.btn_pocisti);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_inch);
+            this.Controls.Add(this.tb_ft);
+            this.Controls.Add(this.tb_cm);
+            this.Controls.Add(this.btn_pretvori);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_pretvori;
+        private System.Windows.Forms.TextBox tb_cm;
+        private System.Windows.Forms.TextBox tb_ft;
+        private System.Windows.Forms.TextBox tb_inch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btn_pocisti;
+    }
+}
+
---
+++ Pretvornik/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Pretvornik
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btn_pretvori.Enabled = false;
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_pretvori_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (tb_cm.Text.Length == 0)
+                {
+                    int ft = Int32.Parse(tb_ft.Text);
+                    int inch = Int32.Parse(tb_inch.Text);
+
+                    tb_cm.Text = (ft * 30.48 + inch * 2.54).ToString();
+                }
+                else
+                {
+                    int cm = Int32.Parse(tb_cm.Text);
+                    int ft = (int)Math.Floor(cm / 30.48);
+                    double inch = 12*((cm / 30.48) - ft);
+
+                    tb_ft.Text = ft.ToString();
+                    tb_inch.Text = inch.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void tb_cm_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tb_cm.Text) && string.IsNullOrEmpty(tb_ft.Text) && string.IsNullOrEmpty(tb_inch.Text))
+            {
+                btn_pretvori.Enabled = false;
+            }
+            else if (string.IsNullOrEmpty(tb_cm.Text) && (string.IsNullOrEmpty(tb_ft.Text) || string.IsNullOrEmpty(tb_inch.Text)))
+            {
+                btn_pretvori.Enabled = false;
+            }
+            else if ((!string.IsNullOrEmpty(tb_ft.Text) && string.IsNullOrEmpty(tb_inch.Text)) || (string.IsNullOrEmpty(tb_ft.Text) && !string.IsNullOrEmpty(tb_inch.Text)))
+            {
+                btn_pretvori.Enabled = false;
+            }
+            else if (!string.IsNullOrEmpty(tb_cm.Text) && !string.IsNullOrEmpty(tb_ft.Text) && !string.IsNullOrEmpty(tb_inch.Text))
+            {
+                btn_pretvori.Enabled = false;
+            }
+            else
+            {
+                btn_pretvori.Enabled = true;
+            }
+        }
+
+        private void tb_ft_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tb_cm.Text) && string.IsNullOrEmpty(tb_ft.Text) && string.IsNullOrEmpty(tb_inch.Text))
+            {
+                btn_pretvori.Enabled = false;
+            }
+            else if (string.IsNullOrEmpty(tb_cm.Text) && (string.IsNullOrEmpty(tb_ft.Text) || string.IsNullOrEmpty(tb_inch.Text)))
+            {
+                btn_pretvori.Enabled = false;
+            }
+            else if ((!string.IsNullOrEmpty(tb_ft.Text) && string.IsNullOrEmpty(tb_inch.Text)) || (string.IsNullOrEmpty(tb_ft.Text) && !string.IsNullOrEmpty(tb_inch.Text)))
+            {
+                btn_pretvori.Enabled = false;
+            }
+            else if (!string.IsNullOrEmpty(tb_cm.Text) && !string.IsNullOrEmpty(tb_ft.Text) && !string.IsNullOrEmpty(tb_inch.Text))
+            {
+                btn_pretvori.Enabled = false;
+            }
+            else
+            {
+                btn_pretvori.Enabled = true;
+            }
+        }
+
+        private void tb_inch_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tb_cm.Text) && string.IsNullOrEmpty(tb_ft.Text) && string.IsNullOrEmpty(tb_inch.Text))
+            {
+                btn_pretvori.Enabled = false;
+            }
+            else if (string.IsNullOrEmpty(tb_cm.Text) && (string.IsNullOrEmpty(tb_ft.Text) || string.IsNullOrEmpty(tb_inch.Text)))
+            {
+                btn_pretvori.Enabled = false;
+            }
+            else if ((!string.IsNullOrEmpty(tb_ft.Text) && string.IsNullOrEmpty(tb_inch.Text)) || (string.IsNullOrEmpty(tb_ft.Text) && !string.IsNullOrEmpty(tb_inch.Text)))
+            {
+                btn_pretvori.Enabled = false;
+            }
+            else if (!string.IsNullOrEmpty(tb_cm.Text) && !string.IsNullOrEmpty(tb_ft.Text) && !string.IsNullOrEmpty(tb_inch.Text))
+            {
+                btn_pretvori.Enabled = false;
+            }
+            else
+            {
+                btn_pretvori.Enabled = true;
+            }
+        }
+
+        private void btn_pocisti_Click(object sender, EventArgs e)
+        {
+            tb_cm.Text = "";
+            tb_ft.Text = "";
+            tb_inch.Text = "";
+        }
+    }
+}
